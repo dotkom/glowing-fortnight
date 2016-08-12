551d41db6541c4aa08574d763a109db812e8e79b
Sardred <andelar96@gmail.com>
2016-08-13 00:34:14 +0200

    Adds dumb fadder component

---
+++ components/fadder/index.js
@@ -0,0 +1,11 @@
+const Fadder = () => {
+  return (
+    <div>
+      <h1>Fadderukene</h1>
+      <p>I fadderukene skal du få bli kjent med både linjeforeningen, NTNU og mange medstudenter. Dette er den viktigste portalen for å stifte bekjentskaper som varer studietiden ut. Vi i Online anbefaler alle å ta del i det supre fadderopplegget vårt!</p>
+      <p>Fadderukene starter etter immatrikuleringen den 16. august.</p>
+    </div>
+  );
+};
+
+export default Fadder;
