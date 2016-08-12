befa6c42e8a6382fddef7d5acf0c9e48a9630899
Sardred <andelar96@gmail.com>
2016-08-13 00:34:53 +0200

    Update the about component

--- components/about/index.js
+++ components/about/index.js
@@ -1,10 +1,9 @@
-const About = ({ text }) => {
+const About = () => {
   return (
     <div>
-      <h1>tjohei</h1>
-      <p>asdf</p>
-
-      { text }
+      <h1>Hvem er Online?</h1>
+      <p>Velkommen til Online, linjeforeningen for informatikkstudenter ved <a href="http://ntnu.no/">NTNU</a>.</p>
+      <p>Det er vi som sørger for at studietiden blir den beste tiden i ditt liv! Vi i Online arrangerer utflukter, turer, fester, holder kurs og bedriftspresentasjoner gjennom hele året.</p>
     </div>
   );
 };
