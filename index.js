1e6597f4a86a0e11db656fd382059243aa00d012
Tri Nguyen <mail@trimn.net>
2016-08-13 01:36:53 +0200

    Update scss includes in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import D from 'react-dom';
 
-require('./styles/main.scss');
+require('./styles/base.scss');
 
 import About from './components/about';
 import Fadder from './components/fadder';
