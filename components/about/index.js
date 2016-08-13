0c7fa994731414c0d28de0604e196a343a00c9c7
Tri Nguyen <mail@trimn.net>
2016-08-13 21:51:25 +0200

    Add go to main page in about section

--- components/about/index.js
+++ components/about/index.js
@@ -4,6 +4,7 @@ const About = () => {
       <img className="header-logo" src="assets/images/online_logo.svg" alt="" />
       <h1>Velkommen til Online, linjeforeningen for informatikkstudenter ved <a href="http://ntnu.no/">NTNU</a>.</h1>
       <p className="about-description">Det er vi som sørger for at studietiden blir den beste tiden i ditt liv! Vi i Online arrangerer utflukter, turer, fester, holder kurs og bedriftspresentasjoner gjennom hele året.</p>
+      <p><button className="about-mainPageButton">Gå til hovedsiden</button></p>
     </header>
   );
 };
