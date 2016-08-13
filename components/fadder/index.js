6851b506967a1328846bce2478035954222eac9e
Tri Nguyen <mail@trimn.net>
2016-08-13 18:57:06 +0200

    Alter fadder section to display two columns

--- components/fadder/index.js
+++ components/fadder/index.js
@@ -1,10 +1,15 @@
 const Fadder = () => {
   return (
     <div id="fadder">
-      <div className="component">
-	<h1>Fadderukene</h1>
-	<p>I fadderukene skal du få bli kjent med både linjeforeningen, NTNU og mange medstudenter. Dette er den viktigste portalen for å stifte bekjentskaper som varer studietiden ut. Vi i Online anbefaler alle å ta del i det supre fadderopplegget vårt!</p>
-	<p>Fadderukene starter etter immatrikuleringen den 16. august.</p>
+      <div className="component fadder-content">
+	<div className="fadder-imageColumn">
+	  <img className="fadder-image" src="assets/images/fadderukene.png" alt="" />
+	</div>
+	<div className="fadder-description">
+	  <h1>Fadderukene.</h1>
+	  <p>I fadderukene skal du få bli kjent med både linjeforeningen, NTNU og mange medstudenter. Dette er den viktigste portalen for å stifte bekjentskaper som varer studietiden ut. Vi i Online anbefaler alle å ta del i det supre fadderopplegget vårt!</p>
+	  <p>Fadderukene starter etter immatrikuleringen den 16. august.</p>
+	</div>
       </div>
     </div>
   );
