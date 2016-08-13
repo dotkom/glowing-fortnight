a60ddbab100220ebf3459dc55c73a5d05edfb28c
Tri Nguyen <mail@trimn.net>
2016-08-13 18:23:17 +0200

    Add class to header description

--- components/about/index.js
+++ components/about/index.js
@@ -3,7 +3,7 @@ const About = () => {
     <header id="header" className="component">
       <img className="header-logo" src="assets/images/online_logo.svg" alt="" />
       <h1>Velkommen til Online, linjeforeningen for informatikkstudenter ved <a href="http://ntnu.no/">NTNU</a>.</h1>
-      <p>Det er vi som sørger for at studietiden blir den beste tiden i ditt liv! Vi i Online arrangerer utflukter, turer, fester, holder kurs og bedriftspresentasjoner gjennom hele året.</p>
+      <p className="about-description">Det er vi som sørger for at studietiden blir den beste tiden i ditt liv! Vi i Online arrangerer utflukter, turer, fester, holder kurs og bedriftspresentasjoner gjennom hele året.</p>
     </header>
   );
 };
