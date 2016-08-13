fbe9d985d15998ddc36aba691b13eaa8773b6d5d
Tri Nguyen <mail@trimn.net>
2016-08-13 02:40:25 +0200

    Alter about/index.js to reflect design

--- components/about/index.js
+++ components/about/index.js
@@ -1,10 +1,10 @@
 const About = () => {
   return (
-    <div>
-      <h1>Hvem er Online?</h1>
-      <p>Velkommen til Online, linjeforeningen for informatikkstudenter ved <a href="http://ntnu.no/">NTNU</a>.</p>
+    <header id="header" className="component">
+      <img className="header-logo" src="assets/images/online_logo.svg" alt="" />
+      <h1>Velkommen til Online, linjeforeningen for informatikkstudenter ved <a href="http://ntnu.no/">NTNU</a>.</h1>
       <p>Det er vi som sørger for at studietiden blir den beste tiden i ditt liv! Vi i Online arrangerer utflukter, turer, fester, holder kurs og bedriftspresentasjoner gjennom hele året.</p>
-    </div>
+    </header>
   );
 };
 
