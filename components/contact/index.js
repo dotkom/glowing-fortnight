060b50aa2ba5498df18c5cf2a70fed3772d5ce9e
Tri Nguyen <mail@trimn.net>
2016-08-13 20:44:26 +0200

    Add content to contact/index

--- components/contact/index.js
+++ components/contact/index.js
@@ -2,8 +2,11 @@ const Contact = () => {
 
   
   return (
-    <div id="social">
-
+    <div id="contact">
+      <div className="component">
+	<h1>Har du noen spørsmål?</h1>
+	<p>Dersom du lurer på noe, ikke nøl med å ta kontakt! Vi kan også nås per mail, <a href="mailto:kontakt@online.ntnu.no">kontakt@online.ntnu.no</a></p>
+      </div>
     </div>
   );
 };
