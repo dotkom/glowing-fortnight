15b2de39960f73c8e962e2efbe354c9392457469
Tri Nguyen <mail@trimn.net>
2016-08-13 20:44:32 +0200

    Add content to social/index

--- components/social/index.js
+++ components/social/index.js
@@ -1,8 +1,11 @@
 const Social = () => {
 
   return (
-    <div id="social">
-
+    <div id="social" className="component">
+      <img className="social-icon" src="assets/images/social/facebook.svg" alt="" />
+      <img className="social-icon" src="assets/images/social/twitter.svg" alt="" />
+      <img className="social-icon" src="assets/images/social/instagram.svg" alt="" />
+      <img className="social-icon" src="assets/images/social/github.svg" alt="" />
     </div>
   );
 };
