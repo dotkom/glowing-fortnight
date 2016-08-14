594856911eaf536de99f82b65ebfbe85f661dd2e
Tri Nguyen <mail@trimn.net>
2016-08-14 09:07:15 +0200

    Add webpack prod plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
   entry: [
@@ -25,5 +26,16 @@ module.exports = {
   sassLoader: {
     includePath: [path.resolve(__dirname, './styles')]
   },
-  plugins: []
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+	'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
 };
