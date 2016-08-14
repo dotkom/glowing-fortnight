848e00f36ca0b9e3cfb8bd505ca8cdd7499c737d
Christian Duvholt <cxhristian@gmail.com>
2016-08-14 05:04:05 +0200

    Fix Sopra Steria 100% width link

--- components/partners/index.js
+++ components/partners/index.js
@@ -1,7 +1,7 @@
 const Partners = () => {
   return (
     <footer id="partners">
-      <a href="http://www.soprasteria.no/"><img className="partner" id="sopraSteria" src="assets/images/partners/soprasteria_white.png" alt="" /></a>
+      <div id="sopraSteria"><a href="http://www.soprasteria.no/"><img className="partner" src="assets/images/partners/soprasteria_white.png" alt="" /></a></div>
       <a href="http://www.bouvet.no/"><img className="partner" src="assets/images/partners/bouvet_white.png" alt="" /></a>
       <a href="http://www.computas.com/"><img className="partner" id="computas" src="assets/images/partners/computas_white.png" alt="" /></a>
       <a href="http://www.knowit.no/"><img className="partner" src="assets/images/partners/knowit_white.png" alt="" /></a>
