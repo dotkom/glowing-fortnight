f74fd5ae1823b50994318d90392f023196efa636
myth <myth@overflow.no>
2016-08-14 05:22:50 +0200

    Change mainPageButton to top-left ribbon. Add scaffold for slack component.

--- components/about/index.js
+++ components/about/index.js
@@ -1,10 +1,10 @@
 const About = () => {
   return (
     <header id="header" className="component">
+      <a href="/" id="about-mainPageButton">Gå til hovedsiden</a>
       <img className="header-logo" src="assets/images/online_logo.svg" alt="" />
       <h1>Velkommen til Online, linjeforeningen for informatikkstudenter ved <a href="http://ntnu.no/">NTNU</a>.</h1>
       <p className="about-description">Det er vi som sørger for at studietiden blir den beste tiden i ditt liv! Vi i Online arrangerer utflukter, turer, fester, holder kurs og bedriftspresentasjoner gjennom hele året.</p>
-      <p><button className="about-mainPageButton">Gå til hovedsiden</button></p>
     </header>
   );
 };
---
+++ components/slack/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by myth on 2016-08-14.
+ */
+
+const Slack =() => {
+  return (
+    <div id="slack"></div>
+  );
+};
+
+export default Slack;
