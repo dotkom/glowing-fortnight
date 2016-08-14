b83d60ac23c7ddc2e6488a1f0a4aea2ac0a9e822
myth <myth@overflow.no>
2016-08-14 10:05:27 +0200

    Set slack api uri back to default

--- common/constants.js
+++ common/constants.js
@@ -3,6 +3,6 @@
  */
 
 const API_EVENTS_URL = '/api/v1/splash-events/?start_time__gte=2016-08-01';
-const API_SLACK_URL = 'https://online.ntnu.no/api/v1/slack/';
+const API_SLACK_URL = '/api/v1/slack/';
 
 export { API_EVENTS_URL, API_SLACK_URL };
