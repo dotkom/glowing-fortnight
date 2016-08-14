29710dc634d3c995ccdae79301aa50999fafaff0
Christian Duvholt <cxhristian@gmail.com>
2016-08-14 05:20:59 +0200

    Render event content using markdown

--- components/calendar/event.js
+++ components/calendar/event.js
@@ -1,16 +1,25 @@
+import Remarkable from 'remarkable';
+
+const markdown = (text) => {
+  var md = new Remarkable();
+  return {
+    __html: md.render(text)
+  };
+};
+
 const Event = ({ title, start_time, content, eventClickHandler, index, active }) => {
   const classes = (index === active) ? 'cal-event-content-active' : '';
-  
+
   return (
     <div onClick={ () => { eventClickHandler(index); } } className="cal-event">
       <div className="cal-event-indicator"></div>
-      
+
       <div className="cal-event-header">
 	<p className="cal-event-dateString">{start_time.toTimeString().substr(0, 5)}</p>
 	<h3 className="cal-title">{title}</h3>
       </div>
-      
-      <p className={`cal-event-content ${ classes }`}>{content}</p><br />
+
+      <p className={`cal-event-content ${ classes }`} dangerouslySetInnerHTML={markdown(content)} /><br />
     </div>
   );
 };
