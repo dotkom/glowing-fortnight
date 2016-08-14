1a7f3c03f321b54aaa7589a47ce3d37339c1ac50
Sardred <andelar96@gmail.com>
2016-08-14 07:18:44 +0200

    Shameless copy and tweak of preDaysButton!

--- components/about/index.js
+++ components/about/index.js
@@ -5,6 +5,7 @@ const About = () => {
       <img className="header-logo" src="assets/images/online_logo.svg" alt="" />
       <h1>Velkommen til Online, linjeforeningen for informatikkstudenter ved <a href="http://ntnu.no/">NTNU</a>.</h1>
       <p className="about-description">Det er vi som sørger for at studietiden blir den beste tiden i ditt liv! Vi i Online arrangerer utflukter, turer, fester, holder kurs og bedriftspresentasjoner gjennom hele året.</p>
+      <a href="#calendar"><button className="skipToCalendar">Hopp til program</button></a>
     </header>
   );
 };
