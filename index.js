1ab8575f373ece438081558ace3e9d162997c9a6
Tri Nguyen <mail@trimn.net>
2016-08-15 18:28:21 +0200

    Remove sample data from index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const App = () => {
     <div>
       <About />
       <Fadder />
-      <Calendar events={sample.results}/>
+      <Calendar />
       <Join />
       <Slack />
       <Social />
@@ -25,38 +25,6 @@ const App = () => {
       <Partners />
     </div>
   );
-};
-
-const sample = {
-  "count": 2,
-  "next": null,
-  "previous": null,
-  "results": [
-    {
-      "title": "Title of first ever event",
-      "content": "kek some keks",
-      "start_time": "2016-08-16T23:00:00Z",
-      "end_time": "2016-08-16T23:00:00Z"
-    },
-    {
-      "title": "Title of day 2 first event",
-      "content": "day 2, kek more keks",
-      "start_time": "2016-08-17T03:00:00Z",
-      "end_time": "2016-08-17T04:00:00Z"
-    },
-    {
-      "title": "Title of day 2 SECOND event",
-      "content": "pretty warmed up on keking now",
-      "start_time": "2016-08-17T05:00:00Z",
-      "end_time": "2016-08-17T06:00:00Z"
-    },
-    {
-      "title": "Title of day 3 only event",
-      "content": "third day, cool it on the keks m8",
-      "start_time": "2016-08-18T10:00:00Z",
-      "end_time": "2016-08-18T12:00:00Z"
-    }
-  ]
 };
 
 D.render(
