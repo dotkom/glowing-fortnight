cfa4b67f3cf688215bbe2be4efed0a4aa0344fc7
Tri Nguyen <mail@trimn.net>
2016-08-15 18:35:18 +0200

    Update class names for more consistency

--- components/calendar/event.js
+++ components/calendar/event.js
@@ -15,8 +15,8 @@ const Event = ({ title, start_time, end_time, content, eventClickHandler, index,
       <div className="cal-event-indicator"></div>
 
       <div className="cal-event-header">
-        <p className="cal-event-dateString">{start_time.toTimeString().substr(0, 5)}</p>
-        <h3 className="cal-title">{title}</h3>
+        <p className="cal-event-date">{start_time.toTimeString().substr(0, 5)}</p>
+        <h3 className="cal-event-title">{title}</h3>
       </div>
 
       <p className={`cal-event-content ${ classes }`} dangerouslySetInnerHTML={markdown(content)}/>
