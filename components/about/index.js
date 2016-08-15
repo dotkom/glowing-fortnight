8efc43cd17a228adef6a5ffcaf1fe238944b74b0
Tri Nguyen <mail@trimn.net>
2016-08-15 17:56:50 +0200

    Add prefix to header-logo selector

--- components/about/index.js
+++ components/about/index.js
@@ -2,7 +2,9 @@ const About = () => {
   return (
     <header id="about" className="component">
       <a href="/" id="about-mainPageButton">Gå til hovedsiden</a>
-      <img className="header-logo" src="assets/images/online_logo.svg" alt="" />
+
+      <img className="about-header-logo" src="assets/images/online_logo.svg" alt="" />
+
       <h1>Velkommen til Online, linjeforeningen for informatikkstudenter ved <a href="http://ntnu.no/">NTNU</a>.</h1>
       <p className="about-description">Det er vi som sørger for at studietiden blir den beste tiden i ditt liv! Vi i Online arrangerer utflukter, turer, fester, holder kurs og bedriftspresentasjoner gjennom hele året.</p>
       <a href="#calendar"><button className="skipToCalendar">Hopp til program</button></a>
