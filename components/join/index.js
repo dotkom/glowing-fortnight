26931c99e8e23224218749bd9cc7a72edb3dd93c
Håkon Solbjørg <hlsolbjorg@gmail.com>
2017-07-02 21:49:51 +0200

    Specify that anyone _can_ join

--- components/join/index.js
+++ components/join/index.js
@@ -2,7 +2,7 @@ const Join = () => {
   return (
     <div id="join" className="component">
       <h1>Bli med i linjeforeningen!</h1>
-      <p>Alle informatikere er med i linjeforeningen. Gå videre til <a href="https://online.ntnu.no/">hovedsiden</a> for å lage din brukerkonto på Online sine systemer.</p>
+      <p>Alle informatikere har rett til å bli medlem av linjeforeningen. Gå videre til <a href="https://online.ntnu.no/">hovedsiden</a> for å opprette din brukerkonto i Online sine systemer.</p>
       <p>Har du lyst til å gjøre studietiden til informatikere enda bedre? Som komitemedlem i Online blir du del av en familie du aldri vil glemme. Vi tilbyr utfordrende og spennende verv i et meget sosialt miljø med stor takhøyde.</p>
       <p>Les mer om de ulike komiteene, linjeforeningen og mye mer på <a href="https://online.ntnu.no/#!about" target="_blank">nettsidene våre</a>.</p>
       <p>Søknadsfrist for komiteopptak er 28. august kl. 23:59. Søknader rettes til <a href="mailto:opptak@online.ntnu.no">opptak@online.ntnu.no</a></p>
