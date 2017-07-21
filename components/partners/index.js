4cb7e4b61fd16fdc04fbc4853cce9414cf64ea34
andelar96@gmail.com <andelar96@gmail.com>
2017-07-21 23:15:55 +0200

    Added logo for Netcompany, made gold partners more clearly golden, and misc tweaks to css

--- components/partners/index.js
+++ components/partners/index.js
@@ -1,10 +1,13 @@
 const Partners = () => {
   return (
     <footer id="partners">
-      <div id="sopraSteria"><a href="http://www.soprasteria.no/"><img className="partner" src="assets/images/partners/soprasteria_white.png" alt="" /></a></div>
-      <a href="http://www.bouvet.no/"><img className="partner" src="assets/images/partners/bouvet_white.png" alt="" /></a>
-      <a href="http://www.computas.com/"><img className="partner" id="computas" src="assets/images/partners/computas_white.png" alt="" /></a>
-      <a href="http://www.knowit.no/"><img className="partner" src="assets/images/partners/knowit_white.png" alt="" /></a>
+      <div id="goldPartners">
+        <a href="https://www.soprasteria.no/"><img className="partner" src="assets/images/partners/soprasteria_white.png" alt="" /></a>
+        <a href="https://www.netcompany.com/no/"><img className="partner" src="assets/images/partners/netcompany_white.png" alt="" /></a>
+      </div>
+      <a href="https://www.bouvet.no/"><img className="partner" src="assets/images/partners/bouvet_white.png" alt="" /></a>
+      <a href="https://www.computas.com/"><img className="partner" id="computas" src="assets/images/partners/computas_white.png" alt="" /></a>
+      <a href="https://www.knowit.no/"><img className="partner" src="assets/images/partners/knowit_white.png" alt="" /></a>
     </footer>
   );
 }
