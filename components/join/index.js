954e482ec8c844ef5b084500a1ea7a936676ce29
Håkon Solbjørg <hlsolbjorg@gmail.com>
2017-08-22 21:12:52 +0200

    Link to supreme guacco

--- components/join/index.js
+++ components/join/index.js
@@ -4,7 +4,7 @@ const Join = () => {
       <h1>Bli med i linjeforeningen!</h1>
       <p>Online er linjeforeningen for alle informatikere. Gå videre til <a href="https://online.ntnu.no/">hovedsiden</a> for å lage brukerkonto og bli medlem.</p>
       <p>Har du lyst til å gjøre studietiden til informatikere enda bedre? Som komitemedlem i Online blir du del av en familie du aldri vil glemme. Vi tilbyr utfordrende og spennende verv i et meget sosialt miljø med stor takhøyde.</p>
-      <p>Les mer om de ulike komiteene, linjeforeningen og mye mer på <a href="https://online.ntnu.no/#!about" target="_blank">nettsidene våre</a>.</p>
+      <p>Les mer om de ulike komiteene på <a href="https://opptak.online.ntnu.no/" target="_blank">opptakssiden vår</a>.</p>
       <p>Søknadsfrist for komiteopptak er 27. august kl. 23:59. Søknader rettes til <a href="mailto:opptak@online.ntnu.no">opptak@online.ntnu.no</a></p>
     </div>
   );
