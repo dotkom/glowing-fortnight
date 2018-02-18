36439f6ab604788d8b0924aca46bbeea7cab01e1
Aslak Hollund <aslakhol@gmail.com>
2018-02-18 03:11:04 +0100

    link to intro to online life article.

--- components/join/index.js
+++ components/join/index.js
@@ -3,6 +3,7 @@ const Join = () => {
     <div id="join" className="component">
       <h1>Bli aktiv i linjeforeningen!</h1>
       <p>Online er linjeforeningen for alle informatikere. Gå videre til <a href="https://online.ntnu.no/">hovedsiden</a> for å lage brukerkonto og bli medlem.</p>
+      <p>Lurer du på hva som skjer i linjeforeningen eller hvem du skal kontakte for å få booke kontoret? En kjapp innføring i ditt liv i Online er å finne på <a href="https://online.ntnu.no/wiki/online/info/sosialt-og-okonomisk/ditt-liv-som-onliner/" target="_blank">wikien</a>.</p>
       <p>Har du lyst til å gjøre studietiden til informatikere enda bedre? Som komitemedlem i Online blir du del av en familie du aldri vil glemme. Vi tilbyr utfordrende og spennende verv i et meget sosialt miljø med stor takhøyde.</p>
       <p>Les mer om de ulike komiteene på <a href="https://opptak.online.ntnu.no/" target="_blank">opptakssiden vår</a>.</p>
       <p>Søknadsfrist for komiteopptak er 26. august kl. 23:59. Søknader rettes til <a href="https://opptak.online.ntnu.no/" target="_blank">opptakssiden</a>.</p>
