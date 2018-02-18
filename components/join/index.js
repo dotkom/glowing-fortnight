1998da54c1b5bed4bef5b63df340267e555ec9d7
Aslak Hollund <aslakhol@gmail.com>
2018-02-18 02:56:56 +0100

    med i -> aktiv i

--- components/join/index.js
+++ components/join/index.js
@@ -1,7 +1,7 @@
 const Join = () => {
   return (
     <div id="join" className="component">
-      <h1>Bli med i linjeforeningen!</h1>
+      <h1>Bli aktiv i linjeforeningen!</h1>
       <p>Online er linjeforeningen for alle informatikere. Gå videre til <a href="https://online.ntnu.no/">hovedsiden</a> for å lage brukerkonto og bli medlem.</p>
       <p>Har du lyst til å gjøre studietiden til informatikere enda bedre? Som komitemedlem i Online blir du del av en familie du aldri vil glemme. Vi tilbyr utfordrende og spennende verv i et meget sosialt miljø med stor takhøyde.</p>
       <p>Les mer om de ulike komiteene på <a href="https://opptak.online.ntnu.no/" target="_blank">opptakssiden vår</a>.</p>
