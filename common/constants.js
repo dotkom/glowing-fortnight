d96ae36d8501d4409187d37a300b5691a93284e0
Aslak Hollund <aslakhol@gmail.com>
2018-06-27 08:53:11 +0200

    update start date

--- common/constants.js
+++ common/constants.js
@@ -1,4 +1,4 @@
-const API_EVENTS_URL = '/api/v1/splash-events/?start_time__gte=2017-08-01';
+const API_EVENTS_URL = '/api/v1/splash-events/?start_time__gte=2018-08-01';
 const API_SLACK_URL = '/api/v1/slack/';
 
 export { API_EVENTS_URL, API_SLACK_URL };
