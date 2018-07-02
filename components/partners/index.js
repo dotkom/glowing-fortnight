cb6046f780871e2cf149a12947ae1e2940f2f9c5
oleast <oleast@stud.ntnu.no>
2018-07-02 20:30:15 +0200

    Add Acando logo and reorganize existing partners

--- components/partners/index.js
+++ components/partners/index.js
@@ -2,12 +2,11 @@ const Partners = () => {
   return (
     <footer id="partners">
       <div id="goldPartners">
+        <a href="https://www.knowit.no/"><img className="partner" src="assets/images/partners/knowit_white.png" alt="" /></a>
         <a href="https://www.soprasteria.no/"><img className="partner" src="assets/images/partners/soprasteria_white.png" alt="" /></a>
-        <a href="https://www.netcompany.com/no/"><img className="partner" src="assets/images/partners/netcompany_white.png" alt="" /></a>
       </div>
-      <a href="https://www.bouvet.no/"><img className="partner" src="assets/images/partners/bouvet_white.png" alt="" /></a>
-      <a href="https://www.computas.com/"><img className="partner" id="computas" src="assets/images/partners/computas_white.png" alt="" /></a>
-      <a href="https://www.knowit.no/"><img className="partner" src="assets/images/partners/knowit_white.png" alt="" /></a>
+      <a href="https://www.netcompany.com/no/"><img className="partner" src="assets/images/partners/netcompany_white.png" alt="" /></a>
+      <a href="https://www.acando.no/"><img className="partner" src="assets/images/partners/acando_white.png" alt="" /></a>
     </footer>
   );
 }
