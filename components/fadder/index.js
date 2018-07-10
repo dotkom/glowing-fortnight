fba50e1a555388eaf0e13a594c8c583a7542f0cb
Tor <tortb@stud.ntnu.no>
2018-07-11 00:04:35 +0200

    Oppdaterte tekst

--- components/fadder/index.js
+++ components/fadder/index.js
@@ -8,7 +8,7 @@ const Fadder = () => {
 	<div className="fadder-description">
 	  <h1>Fadderukene.</h1>
 	  <p>I fadderukene skal du få bli kjent med både linjeforeningen, NTNU og mange medstudenter. Dette er den viktigste portalen for å stifte bekjentskaper som varer studietiden ut. Vi i Online anbefaler alle å ta del i det supre fadderopplegget vårt!</p>
-	  <p>Fadderukene starter den 13. august.</p>
+	  <p>Fadderukene starter den 14. august for bachelor, og den 15. august for master.</p>
 	</div>
       </div>
     </div>
