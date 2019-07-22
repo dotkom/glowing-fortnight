2cac8440df6390f86ffa78bf6b6ece7f284c4aaf
Aslak Hollund <aslakhol@gmail.com>
2019-07-22 22:20:41 +0200

    link to new partners

--- components/partners/index.js
+++ components/partners/index.js
@@ -1,12 +1,12 @@
 const Partners = () => {
   return (
     <footer id="partners">
-      <div id="goldPartners">
+      <div id="generalPartners">
         <a href="https://www.knowit.no/"><img className="partner" src="assets/images/partners/knowit_white.png" alt="" /></a>
         <a href="https://www.soprasteria.no/"><img className="partner" src="assets/images/partners/soprasteria_white.png" alt="" /></a>
       </div>
-      <a href="https://www.netcompany.com/no/"><img className="partner" src="assets/images/partners/netcompany_white.png" alt="" /></a>
-      <a href="https://www.acando.no/"><img className="partner" src="assets/images/partners/acando_white.png" alt="" /></a>
+      <a href="https://www2.deloitte.com/no/no.html"><img className="partner" id="deloitte" src="assets/images/partners/deloitte_white.png" alt="" /></a>
+      <a href="https://www.equinor.com/no.html"><img className="partner" id="equinor" src="assets/images/partners/equinor_white.png" alt="" /></a>
     </footer>
   );
 }
