49389f5e451fe62e18a69f9282f4ae4d6ac9e1db
Aslak Hollund <aslakhol@gmail.com>
2019-07-22 21:36:37 +0200

    update dates for 2019

--- components/fadder/index.js
+++ components/fadder/index.js
@@ -7,8 +7,8 @@ const Fadder = () => {
                     <p>I fadderukene skal du få bli kjent med både linjeforeningen, NTNU og mange medstudenter. Dette er
                         den viktigste portalen for å stifte bekjentskaper som varer studietiden ut. Vi i Online
                         anbefaler alle å ta del i det supre fadderopplegget vårt!</p>
-                    <p>Fadderukene starter den 14. august etter immatrikuleringen for både bachelor og master.</p>
-                    <p>For praktisk info rundt fadderukene se <a href="https://online.ntnu.no/wiki/online/fadderukene/2018-/">her</a></p>
+                    <p>Fadderukene starter den 12. august etter immatrikuleringen for både bachelor og master.</p>
+                    <p>For praktisk info rundt fadderukene se <a href="https://online.ntnu.no/wiki/online/fadderukene/2019-/">her</a></p>
                 </div>
             </div>
         </div>
