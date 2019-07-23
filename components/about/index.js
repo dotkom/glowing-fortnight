e3db41dad7ce0693bec280b9fc934bb17a5fc009
Aslak Hollund <aslak.hollund@iterate.no>
2019-07-23 09:12:11 +0200

    fix type and use ITEX -> IT-ekskursjonen

--- components/about/index.js
+++ components/about/index.js
@@ -8,8 +8,8 @@ const About = () => {
       <h1>Velkommen til Online, linjeforeningen for informatikkstudenter ved <a href="http://ntnu.no/">NTNU</a>.</h1>
       <p className="about-description">Det er vi som sørger for at studietiden blir den beste tiden i ditt liv! Vi i Online arrangerer utflukter, turer, fester, holder kurs og bedriftspresentasjoner gjennom hele året.</p>
 
-      <p>Vi har samlet litt info for nye informatikkere med spørsmål, ta en titt på <a href="https://online.ntnu.no/wiki/online/ny-paa-informatikk/">wikien</a> for å sjekke den ut!</p>
-      <p>Ny på master? Se <a href="https://online.ntnu.no/events/708/it-ekskursjonen-2019/">her</a> for viktig info om ITEX, OBS kort frist! </p>
+      <p>Vi har samlet litt info for nye informatikere med spørsmål, ta en titt på <a href="https://online.ntnu.no/wiki/online/ny-paa-informatikk/">wikien</a> for å sjekke den ut!</p>
+      <p>Ny på master? Se <a href="https://online.ntnu.no/events/708/it-ekskursjonen-2019/">her</a> for viktig info om IT-ekskursjonen, OBS kort frist! </p>
 
       <a href="#calendar" className="skipToCalendar">Hopp til program</a>
     </header>
