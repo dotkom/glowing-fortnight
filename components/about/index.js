d2d3e4968c853dcc269fcf30ee34f2ed07a45767
oleast <oleast@stud.ntnu.no>
2020-03-13 16:33:20 +0100

    Move assets to public to conform to nextjs standards

--- components/about/index.js
+++ components/about/index.js
@@ -3,7 +3,7 @@ const About = () => {
     <header id="about" className="component">
       <a href="/" id="about-mainPageButton">Gå til hovedsiden</a>
 
-      <img className="about-header-logo" src="assets/images/online_logo.svg" alt="" />
+      <img className="about-header-logo" src="/images/online_logo.svg" alt="" />
 
       <h1>Velkommen til Online, linjeforeningen for informatikkstudenter ved <a href="http://ntnu.no/">NTNU</a>.</h1>
       <p className="about-description">Det er vi som sørger for at studietiden blir den beste tiden i ditt liv! Vi i Online arrangerer utflukter, turer, fester, holder kurs og bedriftspresentasjoner gjennom hele året.</p>
--- components/partners/index.js
+++ components/partners/index.js
@@ -2,11 +2,11 @@ const Partners = () => {
   return (
     <footer id="partners">
       <div id="generalPartners">
-        <a href="https://www.knowit.no/"><img className="partner" src="assets/images/partners/knowit_white.png" alt="" /></a>
-        <a href="https://www.soprasteria.no/"><img className="partner" src="assets/images/partners/soprasteria_white.png" alt="" /></a>
+        <a href="https://www.knowit.no/"><img className="partner" src="/images/partners/knowit_white.png" alt="" /></a>
+        <a href="https://www.soprasteria.no/"><img className="partner" src="/images/partners/soprasteria_white.png" alt="" /></a>
       </div>
-      <a href="https://www2.deloitte.com/no/no.html"><img className="partner" id="deloitte" src="assets/images/partners/deloitte_white.png" alt="" /></a>
-      <a href="https://www.equinor.com/no.html"><img className="partner" id="equinor" src="assets/images/partners/equinor_white.png" alt="" /></a>
+      <a href="https://www2.deloitte.com/no/no.html"><img className="partner" id="deloitte" src="/images/partners/deloitte_white.png" alt="" /></a>
+      <a href="https://www.equinor.com/no.html"><img className="partner" id="equinor" src="/images/partners/equinor_white.png" alt="" /></a>
     </footer>
   );
 }
