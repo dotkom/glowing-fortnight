5496342d8681913ef5f9bd66499a50b960c535c2
Henrik Hørlück Berg <henrik@horluck.no>
2020-07-19 13:41:41 +0200

    FIXUP: 7a9647abba1336e2ae68f5b5c67da91eb5838cba

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withSass = require('@zeit/next-sass');
 
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = withSass({
   cssModules: false,
   assetPrefix: isProd ? "/splash/" : "",
