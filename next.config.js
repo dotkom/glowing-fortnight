0eda3f35a7b7717d883d5cf749253a7b73690283
Henrik Hørlück Berg <henrik@horluck.no>
2020-07-19 13:50:57 +0200

    Undo changes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withSass = require('@zeit/next-sass');
 
-const isProd = process.env.NODE_ENV === 'production';
-
 module.exports = withSass({
   cssModules: false,
-  assetPrefix: isProd ? '/splash/' : '',
-  distDir: 'splash/.next',
   cssLoaderOptions: {
     importLoaders: 1,
     localIdentName: '[local]___[hash:base64:5]',
