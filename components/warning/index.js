28694abc0ac55814231389b0e93d3304328b5ea5
Aslak Hollund <aslak.hollund@nav.no>
2020-07-25 00:24:06 +0200

    prettier

--- components/warning/index.js
+++ components/warning/index.js
@@ -5,7 +5,12 @@ const Warning = () => {
     <div id="warning" className="component">
       <h1>Har du opplevd noe ugreit?</h1>
       <p>
-      Online har to kontaktpersoner som kan hjelpe under fadderukene, Thea Karin Fladby (21) og Sindre Langaard (23). Vi ønsker at alle skal ha det bra og føle seg trygge under fadderukene. Derfor håper vi at du tar kontakt dersom du har opplevd noe ubehagelig. Ser du at noen andre opplever noe ubehagelig er det viktig å huske på at du også har et ansvar for å si ifra. Vi tar imot alt, og om du er i tvil er det bare å sende oss en melding. Tar du kontakt med oss vil all informasjon behandles strengt konfidensielt. Vi kan bistå med alt fra en uformell prat til å hjelpe deg med å oppsøke profesjonell hjelp eller rådgivning. 
+        Online har to kontaktpersoner som kan hjelpe under fadderukene, Thea Karin Fladby (21) og Sindre Langaard (23).
+        Vi ønsker at alle skal ha det bra og føle seg trygge under fadderukene. Derfor håper vi at du tar kontakt dersom
+        du har opplevd noe ubehagelig. Ser du at noen andre opplever noe ubehagelig er det viktig å huske på at du også
+        har et ansvar for å si ifra. Vi tar imot alt, og om du er i tvil er det bare å sende oss en melding. Tar du
+        kontakt med oss vil all informasjon behandles strengt konfidensielt. Vi kan bistå med alt fra en uformell prat
+        til å hjelpe deg med å oppsøke profesjonell hjelp eller rådgivning.
       </p>
       <p>
         Om situasjonen tilsier at du trenger å snakke med noen umiddelbart kan du alltid ringe leder Mathias Fossum på
