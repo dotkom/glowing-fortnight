ca156fcdc3cd28223fbbf05234a1af164da5e467
Thomhas <thomas.hasvold@gmail.com>
2021-07-20 21:06:07 +0200

    update images

--- components/partners/index.js
+++ components/partners/index.js
@@ -7,8 +7,8 @@ const Partners = () => {
         <a href="https://bekk.no/">
           <img className="partner" src="/splash/images/partners/bekk_hvit.png" alt="" />
         </a>
-        <a href="https://www.knowit.no/">
-          <img className="partner" src="/splash/images/partners/knowit_white.png" alt="" />
+        <a href="https://www.accenture.com/">
+          <img className="partner" src="/splash/images/partners/accenture_white.png" alt="" />
         </a>
       </div>
       <a href="https://www.soprasteria.no/">
