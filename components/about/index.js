9eaf152b397d2ee9983b032e2f40f6e8283a348c
Njål Sørland <njaalis2000@gmail.com>
2023-08-10 13:25:20 +0200

    add temporary message for students stuck during extreme weather

--- components/about/index.js
+++ components/about/index.js
@@ -35,6 +35,15 @@ const About = () => {
         </a>{' '}
         for viktig info om IT-ekskursjonen. OBS - kort frist!{' '}
       </p>
+      {/* Temporary message added in relation to the extreme weather during the summer of 2023 */}
+      <p>
+        <b>
+          NB! Vi er klar over at ekstremværet &quotHans&quot har gjort det vanskelig for mange å komme seg til
+          Trondheim, men ikke bekymre dere! Det ordner seg uansett, og dere vil ikke miste noen muligheter hos oss.
+          Dersom du har noen spørsmål, ikke nøl med å kontakte oss på{' '}
+          <a href="mailto:kontakt@online.ntnu.no">kontakt@online.ntnu.no</a>. Vi ses!
+        </b>
+      </p>
 
       <a href="#calendar" className="skipToCalendar">
         Hopp til program
